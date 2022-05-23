6570b0216c871e26e25ba4c9110c16a176c7658e
hgoncal2 <aluno23327@ipt.pt>
2022-05-23 21:56:03 +0100

    Inicio da aplicação

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 // App.js
 
+//importar um conteúdo e colocá-lo numa variável
 import logo from './logo.svg';
+//importar fiheiro css
 import './App.css';
 
+
+/* os Componentes podem ser:
+    -classes
+    -funções
+*/
 function App() {
+  // todo o "componente" tem sempre que devolver alguma coisa...
+  // e devolver sempre um único objeto
   return (
-    <div className="App">
+    <div className="App">{/** quando quero referir "CLASS" do CSS,
+     * tenho que usar o termo "className,depois o "JSX" em runtime converte para "class"
+    */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/** no REACT,acede-se ao valor de variáveis atráves de {} */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,8 +32,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>Olá mundo (2ª vez)</h1>
+      <footer className='App-footer'>
+        &copy; app@DW
+      </footer>
     </div>
   );
 }
 
+//só se consegue aceder ao conteúdo do componente,se ele for "exportado"
 export default App;
